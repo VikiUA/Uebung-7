409bc06aed69d81459cfaac475aeeb65258f51b8
Viktoriia <viktoriiapohromska@gmail.com>
2025-12-04 20:03:57 +0100

    End commit

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -5,8 +5,12 @@ import java.util.Arrays;
 public class H2_main {
 
 	public static void main(String[] args) {
-		int[] result = change(new int[] { 2, 7, 1, 9 }, new int[] { 5, 6, 7 }, 2, 4);
-		System.out.println(Arrays.toString(result));
+		int[] a = {2, 7, 1, 9};
+		int[] b = {5, 6, 7};
+		int start = 2;
+		int end = 4;
+		
+		System.out.println(Arrays.toString(change(a, b, start, end)));
 	}
 
 	static public int[] change(int[] a, int[] b, int start, int end) {
@@ -14,14 +18,14 @@ public class H2_main {
 		if (end <= start) {
 			return new int[0];
 		}
-		
+
 		if (Arrays.compare(a, b) != 0) {
 			return Arrays.copyOf(a, a.length);
 		}
-		
+
 		int[] sorted = Arrays.copyOf(a, a.length);
 		Arrays.sort(sorted);
-		
+
 		int[] result = Arrays.copyOfRange(sorted, start, end);
 		return result;
 	}
